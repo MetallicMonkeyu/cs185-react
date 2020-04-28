79f3b13e69d9dd44b1e6473a2de7cf2635f1a503
shengjiayu <jerry.ysj@hotmail.com>
2020-04-27 17:07:30 -0700

    Updates

---
+++ precache-manifest.bb96dcc7cf46eb3d06550e7f80004472.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db26fb75ac0eaa552dcd397c8e2da20",
+    "url": "/cs185-react/index.html"
+  },
+  {
+    "revision": "c6a69be0b15a74f6ef13",
+    "url": "/cs185-react/static/css/main.791f7948.chunk.css"
+  },
+  {
+    "revision": "bd2a6dbc1500f09ba0bf",
+    "url": "/cs185-react/static/js/2.6dec0129.chunk.js"
+  },
+  {
+    "revision": "04007fcd50e895bcbb2b21327be92c6e",
+    "url": "/cs185-react/static/js/2.6dec0129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a69be0b15a74f6ef13",
+    "url": "/cs185-react/static/js/main.ba499895.chunk.js"
+  },
+  {
+    "revision": "139a96c86376f17e55cc",
+    "url": "/cs185-react/static/js/runtime-main.e0d4b5c4.js"
+  },
+  {
+    "revision": "6489c8faaa6c8f6ecdd129595d86b6c5",
+    "url": "/cs185-react/static/media/LOL_replay1.6489c8fa.webm"
+  },
+  {
+    "revision": "e8444f33b1fff4c11043a1544a86dc26",
+    "url": "/cs185-react/static/media/LOL_replay2.e8444f33.webm"
+  },
+  {
+    "revision": "6c68e80e58e6f51ecee04686e02ab40d",
+    "url": "/cs185-react/static/media/LOL_replay3.6c68e80e.webm"
+  },
+  {
+    "revision": "642a37ec1935409f75fae731b0c21b7f",
+    "url": "/cs185-react/static/media/Snake.642a37ec.png"
+  },
+  {
+    "revision": "f14541321c2d742599c2867b52e8efeb",
+    "url": "/cs185-react/static/media/View1.f1454132.jpg"
+  },
+  {
+    "revision": "2a57b4f3dd28d31379f5233fa122ff3b",
+    "url": "/cs185-react/static/media/View10.2a57b4f3.jpg"
+  },
+  {
+    "revision": "269a98a7429d9db8903b9dfa11b91373",
+    "url": "/cs185-react/static/media/View3.269a98a7.jpg"
+  },
+  {
+    "revision": "a47855c15a0618f80c636513e89e738b",
+    "url": "/cs185-react/static/media/View4.a47855c1.jpg"
+  },
+  {
+    "revision": "dd65c52ac00938303b4dd004b3ef11ac",
+    "url": "/cs185-react/static/media/View5.dd65c52a.jpg"
+  },
+  {
+    "revision": "a6aafe35a38ca5269821db242c249c17",
+    "url": "/cs185-react/static/media/View6.a6aafe35.jpg"
+  },
+  {
+    "revision": "d6b71375e49ac6a79e87408f80ad6b13",
+    "url": "/cs185-react/static/media/View7.d6b71375.jpg"
+  },
+  {
+    "revision": "3c122875fe1de841d191bf665ed40263",
+    "url": "/cs185-react/static/media/View8.3c122875.jpg"
+  },
+  {
+    "revision": "212f0c43582acc201b884f0094242aec",
+    "url": "/cs185-react/static/media/View9.212f0c43.jpg"
+  },
+  {
+    "revision": "250b231d6a4989ba507ae9b02db6b0dd",
+    "url": "/cs185-react/static/media/android.250b231d.png"
+  }
+]);
