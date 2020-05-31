c2859076782bdcf976510c63cef7470ca04b59bd
shengjiayu <jerry.ysj@hotmail.com>
2020-05-31 07:30:34 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185-react/precache-manifest.ca851e09e5b03161c3a3861496934e6a.js"
+  "/cs185-react/precache-manifest.b2341c769bc4d609b4ad8fd21ceb5987.js"
 );
 
 self.addEventListener('message', (event) => {
